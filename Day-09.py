4c9ee887e0808937c0a257961e38cc92e7ac64dd
Vivek <86347471+Vivek1105@users.noreply.github.com>
2023-05-02 22:29:26 +0530

    Update Day-09.py

--- Day-09.py
+++ Day-09.py
@@ -14,3 +14,27 @@ class Solution:
             return 1
         else:
             return -1
+
+        
+        
+        
+#         JAVA CODE 
+
+class Solution {
+    public int arraySign(int[] nums) {
+        int n = nums.length;
+        int negatives = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < 0) {
+                negatives++;
+            } else if (nums[i] == 0) {
+                return 0;
+            }
+        }
+        if (negatives % 2 == 0) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+}
