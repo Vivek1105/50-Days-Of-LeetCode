81f03153e51c33793ead05932180ba89237d1cca
Vivek <86347471+Vivek1105@users.noreply.github.com>
2023-05-08 21:40:59 +0530

    Create Day-15.py

---
+++ Day-15.py
@@ -0,0 +1,14 @@
+# Problem - 1572. Matrix Diagonal Sum
+
+
+
+
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        n = len(mat)
+        sum = 0
+        for i in range(n):
+            sum += mat[i][i]
+            if i != n-1-i:
+                sum += mat[i][n-1-i]
+        return sum
